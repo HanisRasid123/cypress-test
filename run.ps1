4a298e42b1e92d31128dffef8fd99a6b4c34d12e
Hanis Rasid <hanis.rasid@healthrfid.com>
2024-01-11 10:52:02 +1100

    add powershell script

---
+++ run.ps1
@@ -0,0 +1,38 @@
+# Function to prompt the user for input
+function Prompt-User {
+    $inputValue = Read-Host "Enter testing stage (SIT/UAT)"
+
+    # Set environment variables based on user input
+    if ($inputValue.ToLower() -eq "sit") {
+        $env:to = 'sit-auto-sanity@healthrfid.com'
+        $env:host = 'https://ecs-v2-sit.controlpoint.healthrfid.com'
+        Write-Host "Environment variables are set to: to=$env:to, host=$env:host"
+    }
+    elseif ($inputValue.ToLower() -eq "uat") {
+        $env:to = 'uat-auto-sanity@healthrfid.com'
+        $env:host = 'https://v2-uat.controlpoint.healthrfid.com'
+        Write-Host "Email recipient will be: $env:to"
+    }
+    elseif ($inputValue.ToLower() -eq "hanis") {
+        $env:to = 'hanis.rasid@healthrfid.com'
+        $env:host = 'https://ecs-v2-sit.controlpoint.healthrfid.com'
+        Write-Host "Environment variables are set to: to=$env:to, host=$env:host"
+    }
+        elseif ($inputValue.ToLower() -eq "ankit") {
+        $env:to ="ankit.saurabh@healthrfid.com"
+        $env:host = 'https://ecs-v2-sit.controlpoint.healthrfid.com'
+        Write-Host "Environment variables are set to: to=$env:to, host=$env:host"
+    }
+    else {
+        Write-Host "Wrong input. Please try again."
+        # Reprompt the user if the input is incorrect
+        Prompt-User
+    }
+}
+
+# Initial prompt
+$env:user = 'AKIA25CJOWA6CPJY7LVH'
+$env:pass = 'BLJgRpABC8l7owAf7TB9gLF3GnL27HWwGM0+Cm8yMp5y'
+Prompt-User
+
+npm run cypress:run
