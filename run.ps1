da173ffc89d5fb5319a99845a5a11829ad47dd27
Hanis Rasid <hanis.rasid@healthrfid.com>
2024-06-13 09:54:24 +1000

    update envhost variable update gitignore

--- run.ps1
+++ run.ps1
@@ -5,22 +5,22 @@ function Prompt-User {
     # Set environment variables based on user input
     if ($inputValue.ToLower() -eq "sit") {
         $env:to = 'sit-auto-sanity@healthrfid.com'
-        $env:host = 'https://ecs-v2-sit.controlpoint.healthrfid.com'
+        $env:host = 'https://sit-vifm.controlpoint.healthrfid.com'
         Write-Host "Environment variables are set to: to=$env:to, host=$env:host"
     }
     elseif ($inputValue.ToLower() -eq "uat") {
         $env:to = 'uat-auto-sanity@healthrfid.com'
-        $env:host = 'https://v2-uat.controlpoint.healthrfid.com'
+        $env:host = 'https://uat-vifm.controlpoint.healthrfid.com'
         Write-Host "Email recipient will be: $env:to"
     }
     elseif ($inputValue.ToLower() -eq "hanis") {
         $env:to = 'hanis.rasid@healthrfid.com'
-        $env:host = 'https://ecs-v2-sit.controlpoint.healthrfid.com'
+        $env:host = 'https://sit-vifm.controlpoint.healthrfid.com'
         Write-Host "Environment variables are set to: to=$env:to, host=$env:host"
     }
         elseif ($inputValue.ToLower() -eq "ankit") {
         $env:to ="ankit.saurabh@healthrfid.com"
-        $env:host = 'https://ecs-v2-sit.controlpoint.healthrfid.com'
+        $env:host = 'https://sit-vifm.controlpoint.healthrfid.com'
         Write-Host "Environment variables are set to: to=$env:to, host=$env:host"
     }
     else {
