b2ad3cbee516f98b62e0834b32eab9d2645dd67d
Hanis Rasid <hanis.rasid@healthrfid.com>
2024-07-08 11:22:20 +1000

    add rfid bay config test

--- cypress/e2e/configuration.cy.js
+++ cypress/e2e/configuration.cy.js
@@ -4,6 +4,7 @@ describe("Configuration", ()=>{
   const locationName = "TESTLOCATION"
   const abbr = "TLOC"
   const readerName = "TESTREADER"
+  const bayName = "TLOC99A"
   beforeEach(()=>{
     cy.login(data.sysAdminEmail, data.sysAdminPassword);
 
@@ -38,7 +39,7 @@ describe("Configuration", ()=>{
     cy.get('form > .modal-card-foot > .button').click()
 
   });
-  it.only("should access RFID Reader Configuration and test", ()=>{
+  it("should access RFID Reader Configuration and test", ()=>{
     cy.get('[data-name="RFID Reader Configuration"]').click()
     //create reader
     cy.get('.create-button').click()
@@ -53,8 +54,30 @@ describe("Configuration", ()=>{
     cy.get('.action-buttons > button').should("be.visible").click()
     cy.get('.activate-prompt-modal > .modal > .modal-card > .modal-card-foot > .button').click()
   });
-  it("should access RFID Bay Configuration and test", ()=>{
+  it.only("should access RFID Bay Configuration and test", ()=>{
     cy.get('[data-name="RFID Bay Configuration"]').click()
+    //create
+    cy.get('.create-button').click()
+    cy.get(':nth-child(2) > .column > .control > .select > #select_locId').select(abbr)
+
+    cy.get('.field > :nth-child(1) > .control > .input').type("99")
+
+    cy.get('.modal-card-body > :nth-child(1) > :nth-child(3) > :nth-child(2) > .field > .control > .select > #select_status').select("A")
+    cy.get(':nth-child(3) > .check').click()
+    cy.get('form > .modal-card-foot > .button').click()
+    //activate/deactivate
+    cy.wait(1000)
+    cy.get(".input").type(bayName)
+    cy.wait(1000)
+    cy.get('[title="Activate/Deactivate Bay"]').click()
+    cy.get('[data-v-5ad3de42=""][data-v-49aa9bd1=""] > .modal > .modal-card > .modal-card-foot > .button').click()
+    cy.get('[title="Activate/Deactivate Bay"]').click()
+    cy.get('[data-v-5ad3de42=""][data-v-49aa9bd1=""] > .modal > .modal-card > .modal-card-foot > .button').click()
+
+    //delete
+    cy.get('[title="Remove Bay"]').click()
+    cy.get('[data-v-26ace4e2=""][data-v-49aa9bd1=""] > .modal > .modal-card > .modal-card-foot > .button').click()
+    
   });
   it("should access BT Gateway Configuration and test", ()=>{
     cy.get('[data-name="BT Gateway Configuration"]').click()
